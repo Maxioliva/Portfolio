81e26396b6944bc49859203d6dd827d3d08f505d
Maximiliano oliva <maxiarieloliva@gmail.com>
2023-05-03 12:32:05 -0300

    Structure

---
+++ src/data/data.tsx
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "Rick and Morty",
+    subtitle: "React js - Typescript",
+    description:
+      "Responsive React SPA, search and display Rick and Morty data.",
+    image: "rickandmorty.png",
+    link: "https://rickandmorty-c137.netlify.app/",
+  },
+  {
+    title: "Movies",
+    subtitle: "React js",
+    description: "Responsive React SPA, search and display Movies data.",
+    image: "movies.png",
+    link: "https://nicemovies.netlify.app/",
+  },
+  {
+    title: "Ecommerce",
+    subtitle: "React js - Typescript - REST - Firebase",
+    description: "Fullstack single-page, responsive, ecommerce application .",
+    image: "ecommerce.png",
+    link: "https://elchamuyin.netlify.app/",
+  },
+];
+
+export const skills = [
+  "ReactJS",
+  "React Native",
+  "Typescrips",
+  "Javascript",
+  "HTML",
+  "CSS",
+  "GrapQL",
+];
